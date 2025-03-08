7640fed561c7f5a72a7b786ccbb035eaa0715d7e
Andrew Thompson <learnaiwithandrew@gmail.com>
2025-03-08 14:36:18 -0600

    removed depricated auth helpers from supabase, added speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 import Header from "@/components/sections/Header";
@@ -51,6 +52,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <Analytics />
+	<SpeedInsights />
       </body>
     </html>
   );
