84e67d797ea584456add00712fa2ecb2afe5f95c
Andrew Thompson <andrew@taltytech.com>
2025-05-12 11:30:45 -0500

    added meta verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+// Vercel
+// TODO: Remove Vercel
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-import "./globals.css";
+// Globals
 import Header from "@/components/globals/Header";
 import Footer from "@/components/globals/Footer";
+import "./globals.css";
 
 // Font Settings for Theme
 const geistSans = Geist({
@@ -24,6 +28,7 @@ export const metadata: Metadata = {
   title: "Talty Tech",
   description:
     "Empowering Texas businesses with innovative web development and AI solutions. Our expert team combines modern design with robust technology to create dynamic ecommerce sites, interactive web apps, and automated business platforms.",
+  facebookDomainVerification: "ptqet2ozqz9vhfnidumn6y6dxqih3x",
   openGraph: {
     title: "Talty Tech",
     description:
